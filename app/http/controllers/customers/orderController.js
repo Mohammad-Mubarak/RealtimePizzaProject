d293e6dc2e5171446e9c6e475deae82c688adc2e
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2022-12-01 17:33:29 +0530

    first commit

---
+++ app/http/controllers/customers/orderController.js
@@ -0,0 +1,46 @@
+const Order = require('../../../models/order')
+const moment = require('moment')
+function orderController () {
+    return {
+        store(req, res) {
+            // Validate request
+            const { phone, address } = req.body
+            if(!phone || !address) {
+                req.flash('dikkat', 'Please fill details to continue')
+                return res.redirect("/cart")
+            }
+
+            const order = new Order({
+                customerId: req.user._id,
+                items: req.session.cart.items,
+                phone,
+                address
+            })
+            order.save().then(result => {
+                    req.flash('success', 'Order placed successfully')
+                    delete req.session.cart
+                    return res.redirect("/customer/orders")
+          
+             } ).catch(err => {
+                return res.status(500).json({ message : 'Something went wrong' });
+            })
+        },
+        async index(req, res) {
+            const orders = await Order.find({ customerId: req.user._id },
+                null,
+                { sort: { 'createdAt': -1 } } )
+            res.header('Cache-Control', 'no-store')
+            res.render('customers/orders', { orders: orders, moment: moment })
+        },
+        async show(req, res) {
+            const order = await Order.findById(req.params.id)
+            // Authorize user
+            if(req.user._id.toString() === order.customerId.toString()) {
+                return res.render('customers/singleOrder', { order })
+            }
+            return  res.redirect('/')
+        }
+    }
+}
+
+module.exports = orderController
